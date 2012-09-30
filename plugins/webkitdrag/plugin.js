0317c5471719120f3dbe7490c916af00007c2f3f
Simon Sturmer <sstur@me.com>
2012-09-30 20:28:28 +0700

    add files

---
+++ plugins/webkitdrag/plugin.js
@@ -0,0 +1,340 @@
+﻿/**
+ * CKEditor plugin: Dragable image resizing for Webkit
+ * - Shows semi-transparent overlay while resizing
+ * - Enforces Aspect Ratio (unless holding shift)
+ * - Snap to size of other images in editor
+ * - Escape while dragging cancels resize
+ *
+ */
+(function() {
+  "use strict";
+
+  var PLUGIN_NAME = 'webkitdrag';
+
+  var IMAGE_SNAP_TO_SIZE = 7;
+
+  /**
+   * Initializes the plugin
+   */
+  CKEDITOR.plugins.add(PLUGIN_NAME, {
+    init: function(editor) {
+      //this plugin only applies to Webkit
+      if (!CKEDITOR.env.webkit) {
+        return;
+      }
+      //onDomReady handler
+      editor.on('contentDom', function(evt) {
+        init(editor);
+      });
+    }
+  });
+
+  function init(editor) {
+    var window = editor.window.$, document = editor.document.$, body = document.body;
+    var snapToSize = (typeof IMAGE_SNAP_TO_SIZE == 'undefined') ? null : IMAGE_SNAP_TO_SIZE;
+
+    addStyle(document, 'img::selection{color:rgba(0,0,0,0);}#ckimgrsz{position:absolute;width:0;height:0;cursor:default;}#ckimgrsz .preview{position:absolute;top:0;left:0;width:0;height:0;background-size:100% 100%;opacity:.65;}#ckimgrsz span{position:absolute;width:5px;height:5px;background:#fff;border:1px solid #000;}#ckimgrsz span:hover,#ckimgrsz span.active{background:#000;}#ckimgrsz span.tl{cursor:nw-resize;}#ckimgrsz span.tm{cursor:n-resize;}#ckimgrsz span.tr{cursor:ne-resize;}#ckimgrsz span.lm{cursor:w-resize;}#ckimgrsz span.rm{cursor:e-resize;}#ckimgrsz span.bl{cursor:sw-resize;}#ckimgrsz span.bm{cursor:s-resize;}#ckimgrsz span.br{cursor:se-resize;}body.dragging-tl,body.dragging-tl *{cursor:nw-resize!important;}body.dragging-tm,body.dragging-tm *{cursor:n-resize!important;}body.dragging-tr,body.dragging-tr *{cursor:ne-resize!important;}body.dragging-lm,body.dragging-lm *{cursor:w-resize!important;}body.dragging-rm,body.dragging-rm *{cursor:e-resize!important;}body.dragging-bl,body.dragging-bl *{cursor:sw-resize!important;}body.dragging-bm,body.dragging-bm *{cursor:s-resize!important;}body.dragging-br,body.dragging-br *{cursor:se-resize!important;}');
+
+    function DragEvent() {
+      this.events = {
+        mousemove: bind(this.mousemove, this),
+        keydown: bind(this.keydown, this),
+        mouseup: bind(this.mouseup, this)
+      };
+    }
+
+    DragEvent.prototype = {
+      start: function(e) {
+        if (e.button !== 0) {
+          //right-click or middle-click
+          return;
+        }
+        e.preventDefault();
+        e.stopPropagation();
+        this.target = e.target;
+        this.startPos = {x: e.clientX, y: e.clientY};
+        this.update(e);
+        var events = this.events;
+        document.addEventListener('mousemove', events.mousemove, false);
+        document.addEventListener('keydown', events.keydown, false);
+        document.addEventListener('mouseup', events.mouseup, false);
+        body.className += ' dragging-' + this.attr
+        this.onStart && this.onStart();
+      },
+      update: function(e) {
+        this.currentPos = {x: e.clientX, y: e.clientY};
+        this.delta = {x: e.clientX - this.startPos.x, y: e.clientY - this.startPos.y};
+        this.keys = {shift: e.shiftKey, ctrl: e.ctrlKey, alt: e.altKey};
+      },
+      mousemove: function(e) {
+        this.update(e);
+        this.onDrag && this.onDrag();
+        if (e.which === 0) {
+          //mouse button released outside window; mouseup wasn't fired (Chrome)
+          this.mouseup(e);
+        }
+      },
+      keydown: function(e) {
+        //escape key cancels dragging
+        if (e.keyCode == 27) {
+          this.release();
+        }
+      },
+      mouseup: function(e) {
+        this.update(e);
+        this.release();
+        this.onComplete && this.onComplete();
+      },
+      release: function() {
+        body.className = body.className.replace('dragging-' + this.attr, '');
+        var events = this.events;
+        document.removeEventListener('mousemove', events.mousemove, false);
+        document.removeEventListener('keydown', events.keydown, false);
+        document.removeEventListener('mouseup', events.mouseup, false);
+        this.onRelease && this.onRelease();
+      }
+    };
+
+    function Resizer(element) {
+      this.el = element;
+      this.events = {
+        initDrag: bind(this.initDrag, this)
+      };
+      this.init();
+      this.show();
+    }
+
+    Resizer.prototype = {
+      init: function() {
+        var old = Resizer.instance;
+        Resizer.instance = this;
+        if (old) {
+          //reuse existing dom elements
+          old.hide();
+          this.container = old.container;
+          this.preview = old.preview;
+          this.handles = old.handles;
+          return;
+        }
+        var container = this.container = document.createElement('div');
+        container.id = 'ckimgrsz';
+        var preview = this.preview = document.createElement('div');
+        preview.className = 'preview';
+        container.appendChild(preview);
+        var handles = this.handles = {};
+        handles.tl = document.createElement('span');
+        handles.tl.className = 'tl';
+        handles.tm = document.createElement('span');
+        handles.tm.className = 'tm';
+        handles.tr = document.createElement('span');
+        handles.tr.className = 'tr';
+        handles.lm = document.createElement('span');
+        handles.lm.className = 'lm';
+        handles.rm = document.createElement('span');
+        handles.rm.className = 'rm';
+        handles.bl = document.createElement('span');
+        handles.bl.className = 'bl';
+        handles.bm = document.createElement('span');
+        handles.bm.className = 'bm';
+        handles.br = document.createElement('span');
+        handles.br.className = 'br';
+        for (var n in handles) {
+          container.appendChild(handles[n]);
+        }
+      },
+      show: function() {
+        if (snapToSize) {
+          this.otherImages = toArray(document.getElementsByTagName('img'));
+          this.otherImages.splice(this.otherImages.indexOf(this.el), 1);
+        }
+        var box = this.box = getBoundingBox(window, this.el);
+        this.container.style.left = box.left + 'px';
+        this.container.style.top = box.top + 'px';
+        body.appendChild(this.container);
+        this.showHandles();
+      },
+      hide: function() {
+        if (this.container.parentNode) {
+          this.hideHandles();
+          this.container.parentNode.removeChild(this.container);
+        }
+      },
+      initDrag: function(e) {
+        var resizer = this;
+        var drag = new DragEvent();
+        drag.onStart = function() {
+          resizer.hideHandles();
+          resizer.showPreview();
+          resizer.isDragging = true;
+          editor.getSelection().lock();
+        };
+        drag.onDrag = function() {
+          resizer.calculateSize(this);
+          resizer.updatePreview();
+        };
+        drag.onRelease = function() {
+          resizer.isDragging = false;
+          resizer.hidePreview();
+          resizer.hide();
+          editor.getSelection().unlock();
+          //editor.focus() will trigger selection change which may start a whole new resize event
+          editor.focus();
+        };
+        drag.onComplete = function() {
+          resizer.resizeComplete();
+        };
+        drag.start(e);
+      },
+      showHandles: function() {
+        var handles = this.handles, box = this.box;
+        handles.tl.style.left = '-3px';
+        handles.tl.style.top = '-3px';
+        handles.tm.style.left = (Math.round(box.width / 2) - 3) + 'px';
+        handles.tm.style.top = '-3px';
+        handles.tr.style.left = (box.width - 4) + 'px';
+        handles.tr.style.top = '-3px';
+        handles.lm.style.left = '-3px';
+        handles.lm.style.top = (Math.round(box.height / 2) - 3) + 'px';
+        handles.rm.style.left = (box.width - 4) + 'px';
+        handles.rm.style.top = (Math.round(box.height / 2) - 3) + 'px';
+        handles.bl.style.left = '-3px';
+        handles.bl.style.top = (box.height - 4) + 'px';
+        handles.bm.style.left = (Math.round(box.width / 2) - 3) + 'px';
+        handles.bm.style.top = (box.height - 4) + 'px';
+        handles.br.style.left = (box.width - 4) + 'px';
+        handles.br.style.top = (box.height - 4) + 'px';
+        for (var n in handles) {
+          handles[n].style.display = 'block';
+          handles[n].addEventListener('mousedown', this.events.initDrag, false);
+        }
+      },
+      hideHandles: function() {
+        var handles = this.handles;
+        for (var n in handles) {
+          handles[n].removeEventListener('mousedown', this.events.initDrag, false);
+          handles[n].style.display = 'none';
+        }
+      },
+      showPreview: function() {
+        this.preview.style.backgroundImage = 'url("' + this.el.src + '")';
+        this.calculateSize();
+        this.updatePreview();
+        this.preview.style.display = 'block';
+      },
+      updatePreview: function() {
+        var box = this.previewBox;
+        this.preview.style.top = box.top + 'px';
+        this.preview.style.left = box.left + 'px';
+        this.preview.style.width = box.width + 'px';
+        this.preview.style.height = box.height + 'px';
+      },
+      hidePreview: function() {
+        var box = getBoundingBox(window, this.preview);
+        this.result = {width: box.width, height: box.height};
+        this.preview.style.display = 'none';
+      },
+      calculateSize: function(data) {
+        var box = this.previewBox = {top: 0, left: 0, width: this.box.width, height: this.box.height};
+        if (!data) return;
+        var attr = data.target.className;
+        if (~attr.indexOf('r')) {
+          box.width = Math.max(32, this.box.width + data.delta.x);
+        }
+        if (~attr.indexOf('b')) {
+          box.height = Math.max(32, this.box.height + data.delta.y);
+        }
+        if (~attr.indexOf('l')) {
+          box.width = Math.max(32, this.box.width - data.delta.x);
+        }
+        if (~attr.indexOf('t')) {
+          box.height = Math.max(32, this.box.height - data.delta.y);
+        }
+        //if dragging corner, enforce aspect ratio (unless shift key is being held)
+        if (attr.indexOf('m') < 0 && !data.keys.shift) {
+          var ratio = this.box.width / this.box.height;
+          if (box.width / box.height > ratio) {
+            box.height = Math.round(box.width / ratio);
+          } else {
+            box.width = Math.round(box.height * ratio);
+          }
+        }
+        if (snapToSize) {
+          var others = this.otherImages;
+          for (var i = 0; i < others.length; i++) {
+            var other = getBoundingBox(window, others[i]);
+            if (Math.abs(box.width - other.width) <= snapToSize && Math.abs(box.height - other.height) <= snapToSize) {
+              box.width = other.width;
+              box.height = other.height;
+              break;
+            }
+          }
+        }
+        //recalculate left or top position
+        if (~attr.indexOf('l')) {
+          box.left = this.box.width - box.width;
+        }
+        if (~attr.indexOf('t')) {
+          box.top = this.box.height - box.height;
+        }
+      },
+      resizeComplete: function() {
+        this.el.style.width = this.result.width + 'px';
+        this.el.style.height = this.result.height + 'px';
+      }
+    };
+    Resizer.hide = function() {
+      this.instance && this.instance.hide();
+    };
+
+    editor.on('selectionChange', function(evt) {
+      var data = evt.data;
+      //if an element is selected and that element is an IMG
+      if (data.selection.getType() != CKEDITOR.SELECTION_NONE && data.element.is('img')) {
+        new Resizer(data.element.$);
+      } else {
+        Resizer.hide();
+      }
+    });
+    editor.on('beforeModeUnload', function self() {
+      editor.removeListener('beforeModeUnload', self);
+      Resizer.hide();
+    });
+  }
+
+
+  //helper functions
+  function toArray(obj) {
+    var len = obj.length, arr = new Array(len);
+    for (var i = 0; i < len; i++) {
+      arr[i] = obj[i];
+    }
+    return arr;
+  }
+
+  function bind(fn, ctx) {
+    if (fn.bind) {
+      return fn.bind(ctx);
+    }
+    return function() {
+      fn.apply(ctx, arguments);
+    };
+  }
+
+  function getBoundingBox(window, el) {
+    var rect = el.getBoundingClientRect();
+    return {
+      left: rect.left + window.pageXOffset,
+      top: rect.top + window.pageYOffset,
+      width: rect.width,
+      height: rect.height
+    };
+  }
+
+  function addStyle(document, css) {
+    var head = document.getElementsByTagName('head')[0];
+    var style = document.createElement('style');
+    style.appendChild(document.createTextNode(css));
+    head.appendChild(style);
+  }
+
+})();
---
+++ readme.md
@@ -0,0 +1,19 @@
+#CKEditor plugin: Dragable image resizing for Webkit
+
+This plugin implements draggable image resizing in Webkit-based browsers (Chrome/Safari). The feature already
+exists in Internet Explorer and Firefox as a built-in browser capability but not in Webkit. So if you or your
+website's users are used to seeing the small drag-to-resize handles at the corner of images, but use Chrome
+or Safari, install this plugin to get it back (with a few bonus features).
+
+##Features:
+ * Shows semi-transparent overlay while resizing
+ * Enforces Aspect Ratio (unless holding shift)
+ * Snap to size of other images in editor (optional)
+ * Escape while dragging cancels resize
+
+I have implemented this feature in pure JavaScript with no external dependencies. It only activates if a Webkit
+browser is detected and it has been tested in most recent versions of Chrome and Safari on PC and Mac.
+
+Please, if you notice any bugs, open an issue in the [issue tracker](/sstur/ck-webkitdrag/issues).
+
+Feel free to use this in commercial or non-comercial projects and fork and modify it at will.
